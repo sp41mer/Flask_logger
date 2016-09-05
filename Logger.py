6a225a5779811624ded89cd49f5312a98324bf91
sp41mer <sp41mer@yahoo.com>
2016-09-05 19:21:46 +0300

    made 2 routes

--- Logger.py
+++ Logger.py
@@ -5,8 +5,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
-def hello_world():
+@app.route('/log/', methods=['GET','POST'])
+def write_to_log():
     method = request.method
     header = request.headers
     query = request.query_string.decode('utf-8')
@@ -14,6 +14,10 @@ def hello_world():
                     'HEADERS:\n {}'
                     'QUERY STRING:\n {}\n'.format(method,header,query))
     return 'Logger_is_running'
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
 
 
 if __name__ == '__main__':
